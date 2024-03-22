c96389d73c9ac0e35514e254479f881326486bb0
Lea-Gosselin <lea_gosselin@outlook.fr>
2024-03-22 22:26:09 +0100

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+// Fonction pour ouvrir l'onglet sélectionné
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Ouvre le premier onglet par défaut
+document.getElementById("Tab1").style.display = "block";
+
+// Attache un gestionnaire d'événement à chaque bouton d'onglet pour appeler la fonction openTab lorsque le bouton est cliqué
+var tabButtons = document.querySelectorAll(".tab button");
+tabButtons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        openTab(event, this.getAttribute("data-tab")); // Appelle la fonction openTab avec le nom de l'onglet correspondant
+    });
+});
